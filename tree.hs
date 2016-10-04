da77dee1ef48605cb349df225842f728778b8a62
Farzad FARID <ffarid@pragmatic-source.com>
2016-10-04 16:04:34 +0200

    Add sorted insertion

--- tree.hs
+++ tree.hs
@@ -4,6 +4,13 @@
 
 data Tree = Leaf | Node Int Tree Tree deriving Show
 
+-- Sample trees
+tsorted = Node 5 (Node 1 Leaf Leaf) (Node 7 Leaf Leaf)
+t = (Node 3 (Node 5 (Node 8 Leaf Leaf) Leaf) 
+            (Node 7 (Node 2 Leaf Leaf) (Node 9 Leaf Leaf)))
+
+-------------------------------------------------------
+
 treeDepth :: Tree -> Int
 treeDepth Leaf = 0
 treeDepth (Node _ leftSubtree rightSubtree) = 
@@ -60,6 +67,24 @@ tree2list'' (Node x t1 t2) = (tree2list'' t1) ++ (tree2list'' t2) ++ [x]
 
 -- tree2list'' t
 
-t = (Node 3 (Node 5 (Node 8 Leaf Leaf) Leaf) 
-            (Node 7 (Node 2 Leaf Leaf) (Node 9 Leaf Leaf)))
+---------------------------------------------------------------------------------
 
+-- Insert Node in order
+insertOrdered :: Tree -> Int -> Tree
+insertOrdered Leaf n = Node n Leaf Leaf
+insertOrdered (Node x t1 t2) n
+  | n == x = Node x (Node n t1 Leaf) t2
+  | n < x  = Node x (insertOrdered t1 n) t2
+  | n > x  = Node x t1 (insertOrdered t2 n)
+
+-- *Main> insertOrdered tsorted 2
+-- Node 5 (Node 1 Leaf (Node 2 Leaf Leaf)) (Node 7 Leaf Leaf)
+
+-- *Main> insertOrdered tsorted 6
+-- Node 5 (Node 1 Leaf Leaf) (Node 7 (Node 6 Leaf Leaf) Leaf)
+
+-- *Main> insertOrdered tsorted 8
+-- Node 5 (Node 1 Leaf Leaf) (Node 7 Leaf (Node 8 Leaf Leaf))
+
+-- *Main> insertOrdered tsorted 5
+-- Node 5 (Node 5 (Node 1 Leaf Leaf) Leaf) (Node 7 Leaf Leaf)
